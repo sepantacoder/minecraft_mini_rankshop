ab6df2b6a907bc76cfd63e3ccf5cba72cca90b25
Sepanta Asadi <sepanta.ipadair11@gmail.com>
2026-08-23 16:13:28 +0330

    picture bug

--- config/config/settings.py
+++ config/config/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
