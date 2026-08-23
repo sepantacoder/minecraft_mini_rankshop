c1028ce4957fb16b5b37c48797f36ffb7f1f4ae6
Sepanta Asadi <sepanta.ipadair11@gmail.com>
2026-08-23 16:40:47 +0330

    picture fixed?

--- config/config/urls.py
+++ config/config/urls.py
@@ -19,7 +19,6 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from store import views
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",views.home,name="home"),
