b4e4e141837d14ba992fd70ff5a63a916d2d1f44
Sepanta Asadi <sepanta.ipadair11@gmail.com>
2026-08-23 09:15:28 +0330

    Fix Render allowed hosts

--- config/config/settings.py
+++ config/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d*(9$!o4)f_w6b__lxf$rnmgei&m-enlyefap)xv1z&=w8(f3z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = [
-    "minecraft-mini-rankshop-5.onrender.com",
+    ".onrender.com",
     "127.0.0.1",
     "localhost",
 ]
